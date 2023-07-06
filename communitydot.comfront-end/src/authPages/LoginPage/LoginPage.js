67c20c7de924ac35702df3c455988785bdd0391a
Parth <ps2002parth@gmail.com>
2023-07-06 21:39:36 +0530

    first commit

---
+++ communitydot.comfront-end/src/authPages/LoginPage/LoginPage.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import AuthBox from "../../shared/components/AuthBox";
+import LoginPageFooter from "./LoginPageFooter";
+import LoginPageHeader from "./LoginPageHeader";
+import LoginPageInputs from "./LoginPageinputs";
+import { validateLoginForm } from "../../shared/components/utlis/validators";
+import {connect} from "react-redux";
+import {getActions} from '..//../store/actions/authActions';
+import { useHistory } from "react-router-dom";
+const LoginPage = ({ login }) => {
+  const history = useHistory();
+
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
+  const handleLogin = () => {
+    const userDetails = {
+      mail,
+      password,
+    };
+
+    login(userDetails, history);
+  };
+
+  return (
+    <AuthBox>
+      <LoginPageHeader />
+      <LoginPageInputs
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+      />
+      <LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin} />
+    </AuthBox>
+  );
+};
+
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch),
+  };
+};
+
+export default connect(null, mapActionsToProps)(LoginPage);
